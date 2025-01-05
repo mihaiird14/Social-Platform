eb393b7846ea90c68922c4433dc52e0bf43640ff
mihaiird14 <mihaiird14@gmail.com>
2025-01-05 18:34:27 +0200

    fixes_posts

--- obj/Debug/net8.0/Social Life.AssemblyInfo.cs
+++ obj/Debug/net8.0/Social Life.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Social Life")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4768c7b6876c300007ac3819223ce2a944151faa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e0c9fb71e397941523b15731a88112ad140c129")]
 [assembly: System.Reflection.AssemblyProductAttribute("Social Life")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Social Life")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
